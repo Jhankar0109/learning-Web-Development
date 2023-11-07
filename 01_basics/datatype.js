626dc377cd0bacb5e8506b798dd68d31643e34a8
Jhankar Niyogi <jhankar.niyogi0109@gmail.com>
2023-11-07 06:16:48 +0000

    container config done

---
+++ 01_basics/datatype.js
@@ -0,0 +1,5 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3+3); we are using node js and not browser
+
+console.log(2+2);
---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountEmail = "jhankar@jhankar.com"
+var accountpassword = "12345567"
+accountCity = "Bengaluru"
+
+let accountState;
+
+// accountId = 1234 we do not use var due to block scope issue and functional scope
+
+console.table([accountEmail,accountId,accountpassword, accountCity, accountState]);
